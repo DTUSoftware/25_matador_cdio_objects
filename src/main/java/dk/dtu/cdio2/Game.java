6ea8eacd75812af2eb11922dcb6d638d8252d9a4
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2021-10-15 11:12:05 +0200

    Small chances in comments

--- src/main/java/dk/dtu/cdio2/Game.java
+++ src/main/java/dk/dtu/cdio2/Game.java
@@ -1,8 +1,8 @@
 package dk.dtu.cdio2;
 
-//***************************************************************//
-// Main class for the program
-//***************************************************************//
+/**
+ * Main class for the program
+ */
 public class Game {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
--- src/main/java/dk/dtu/cdio2/PlayerManager.java
+++ src/main/java/dk/dtu/cdio2/PlayerManager.java
@@ -4,29 +4,31 @@ import java.util.HashMap;
 
 public class PlayerManager {
 
-    //***************************************************************//
-    // We create a HashMap to keep the players and their numbers.
-    //***************************************************************//
+    /**
+     * We create a HashMap to keep the players and their numbers.
+     */
+
     private HashMap<Integer, Player> players = new HashMap<>();
 
     class Player {
 
         private final String name;
+        private int money = 0;
 
         Player (String name) {
             this.name = name;
         }
 
         public void withdrawMoney(int m) {
-            Money += m;
+            money += m;
         }
 
         public void setMoney(int newMoney) {
-            Money = newMoney;
+            money = newMoney;
         }
 
         public int getMoney() {
-            return Money;
+            return money;
         }
 
 

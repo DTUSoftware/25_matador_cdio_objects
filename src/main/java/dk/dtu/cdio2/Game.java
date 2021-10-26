b55857521720b48963a330c64e97a46f11f12570
HeroGamers <hero@herogamers.dev>
2021-10-26 14:49:45 +0200

    v0.2.0 - Danish and English language changing

--- src/main/java/dk/dtu/cdio2/Game.java
+++ src/main/java/dk/dtu/cdio2/Game.java
@@ -6,39 +6,45 @@ import dk.dtu.cdio2.actions.*;
  * Main class for the program
  */
 public class Game {
-    private final static GUIManager gm = new GUIManager();
+    private final static LanguageManager lm = new LanguageManager();
+    private final static GUIManager gm = new GUIManager(lm);
     private final static PlayerManager pm = new PlayerManager(gm);
     private final static DiceManager dm = new DiceManager();
     private final static ActionManager am = new ActionManager(dm, gm, pm);
 
     private final static int startingBalance = 1000;
+    private final static int finalBalance = 3000;
     private static boolean isPlaying = true;
-    private final static boolean debug = ((System.getenv("debug") != null) || (System.getProperty("debug") != null));
+    public final static boolean debug = ((System.getenv("debug") != null) || (System.getProperty("debug") != null));
 
     public static void main(String[] args) {
+        // Choose a language
+        if (gm.askLanguage()) {
+            lm.setLocale("en");
+        }
+        else {
+            lm.setLocale("da");
+        }
+        gm.reloadLanguage();
+
         // Create the players
         for (int i = 1; i <= 2; i++) {
-            String playerName = gm.getUserString("Enter name for Player " + i);
+            String playerName = gm.getUserString(lm.getString("enter_player_name").replace("{player_number}", Integer.toString(i)));
             PlayerManager.Player player = pm.createPlayer(playerName, startingBalance);
             gm.createGUIPlayer(player.getID(), playerName, player.getMoney());
         }
 
-        //***************************************************************//
-        // Play the game until they want to stop playing.
-        //***************************************************************//
+        // Play the game until they want to stop playing
         while (isPlaying) {
             play();
         }
 
-        //***************************************************************//
-        // After they have stopped playing, close the GUI and exit.
-        //***************************************************************//
+        // After they have stopped playing, close the GUI and exit
         gm.closeGUI();
         System.exit(0);
     }
 
     private static void play() {
-        DiceManager.DiceCup dc = dm.createDiceCup();
         Action rollAction = am.getAction(1);
 
         int playerWon = 0;
@@ -50,15 +56,15 @@ public class Game {
                 rollAction.doAction(playerID);
 
                 // Check wincondition
-                if (player.getMoney() >= 3000) {
+                if (player.getMoney() >= finalBalance) {
                     playerWon = playerID;
-                    gm.showMessage(player.getName() + " has reached a balance of 3000 and has won the game!");
+                    gm.showMessage(lm.getString("game_win").replace("{player_name}", player.getName()));
                     break;
                 }
             }
         }
 
-        if (gm.askPrompt("Do you want to play again?")) {
+        if (gm.askPrompt(lm.getString("play_again"))) {
             for (int playerID : pm.getPlayerIDs()) {
                 pm.getPlayer(playerID).setMoney(startingBalance);
             }
